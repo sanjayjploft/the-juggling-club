ef639d523043d72f19a4a5db04cc0bf83b1f7b2c
sanjayjploft <sanjayxdjploft@gmail.com>
2026-02-12 19:50:09 +0530

    updated code

---
+++ src/app/(clubpanel)/club-settings/page.js
@@ -0,0 +1,137 @@
+"use client";
+import { useState } from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import CoachSidebar from "../../../components/dashboard/CoachSidebar";
+import Link from "next/link";
+
+export default function DashboardPage() {
+  const [copied, setCopied] = useState(false);
+  const joinCode = "T-SAE-12345";
+
+  const copyCode = () => {
+    navigator.clipboard.writeText(joinCode);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+  return (
+    <div className="msar-dashboard-wrapper">
+      <CoachSidebar />
+
+      <main className="msar-main">
+        {/* HEADER */}
+        <div className="msar-top-bar d-flex justify-content-between align-items-start">
+          <div className="top-heading">
+            <div>
+              <h4>Club Settings</h4>
+              <p className="mb-0">
+                {" "}
+                Manage your club profile and contact information
+              </p>
+            </div>
+          </div>
+          <div className="user-profile">
+            <Link href="#">
+              <img src="/assets/image/coach-mike.png" />
+            </Link>
+          </div>
+        </div>
+
+        {/* CONTENT */}
+        <div className="container-fluid ">
+          <div className="player-card mx-3 mb-4">
+            <div className="settings-sub-heading">
+              <h6>Pricing Tiers</h6>
+              <p className="">Club subscription plans based on player count</p>
+            </div>
+            <Row>
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"Manchester United Youth Academy"} />
+                  <label>Club Name</label>
+                </Form.Floating>
+              </Col>
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"https://www.manchesterunited.com"} />
+                  <label>Website</label>
+                </Form.Floating>
+              </Col>
+
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"admin@mufc.com"} />
+                  <label>Contact Email</label>
+                </Form.Floating>
+              </Col>
+
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"+44 161 676 1994"} />
+                  <label>Phone</label>
+                </Form.Floating>
+              </Col>
+            </Row>
+          </div>
+          <div className="player-card mx-3 mb-4">
+            <div className="settings-sub-heading">
+              <h6>Location</h6>
+              <p className="">Club headquarters address</p>
+            </div>
+            <Row>
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control
+                    value={"Carrington Training Ground, Carrington"}
+                  />
+                  <label>Street Address</label>
+                </Form.Floating>
+              </Col>
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"Manchester"} />
+                  <label>City</label>
+                </Form.Floating>
+              </Col>
+
+              <Col md={5}>
+                <Form.Floating className="mb-4">
+                  <Form.Control value={"England"} />
+                  <label>Country</label>
+                </Form.Floating>
+              </Col>
+            </Row>
+          </div>
+          <div className="player-card mx-3">
+            <div className="settings-sub-heading">
+              <h6>Notification Preferences</h6>
+              <p className="">Club headquarters address</p>
+            </div>
+            <div className="notification-card notification-card2">
+              <div className="notification-card-in">
+                <h3>Weekly summary email</h3>
+                <input class="switch" type="checkbox" />
+              </div>
+            </div>
+            <div className="notification-card notification-card2">
+              <div className="notification-card-in">
+                <h3>Billing alerts</h3>
+                <input class="switch" type="checkbox" />
+              </div>
+            </div>
+            <div className="notification-card notification-card2">
+              <div className="notification-card-in">
+                <h3>Player inactivity alerts</h3>
+                <input class="switch" type="checkbox" />
+              </div>
+            </div>
+          </div>
+          <div className="mt-4 mx-3">
+            <Link href="" className="btn-primary">
+              Save Settings
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
