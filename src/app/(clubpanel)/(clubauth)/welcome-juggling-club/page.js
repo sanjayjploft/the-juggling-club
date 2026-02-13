b49b2362a95ff9bc8477ee380c609f4950099b72
sanjayjploft <sanjayxdjploft@gmail.com>
2026-02-13 21:09:02 +0530

    updated code

--- src/app/(clubpanel)/(clubauth)/welcome-juggling-club/page.js
+++ src/app/(clubpanel)/(clubauth)/welcome-juggling-club/page.js
@@ -1,26 +1,22 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function ClubInformation() {
-  const router = useRouter();
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push("/dashboardclub");
-    }, 5000); // 5 seconds
+      window.location.href = "/dashboardclub"; // redirect + reload
+    }, 5000);
 
     return () => clearTimeout(timer);
-  }, [router]);
+  }, []);
+
   return (
     <>
-      {/* Steps */}
-
-      {/* Form */}
-
       <div className="welcome-box">
-        <img src="/assets/image/checkmark-confrm.svg" />
+        <img src="/assets/image/checkmark-confrm.svg" alt="success" />
         <h4>Welcome to Juggling Club!</h4>
         <span>Your club has been successfully registered </span>
+
         <div className="club-code">
           <span>Your club code:</span>
           <h6>C-WLW-785477</h6>
@@ -30,6 +26,7 @@ export default function ClubInformation() {
           </p>
         </div>
       </div>
+
       <div className="redirecting">Redirecting to your dashboard...</div>
     </>
   );
